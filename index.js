ba4fbbe4ab2e1d4b1766ca1e0b2635092bd95d7c
khoa <agnus1904@gmail.com>
2020-07-30 02:20:58 +0700

    add some comment in index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
+// express js
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// require module route
 var userRoutes = require("./route/user.route");
+
 // use pug engine
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-//use req.body
+//use req.body in express js
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// home page
 app.get('/', (req, res) => res.render("index"));
 
+// user page
 app.use("/users",userRoutes);
 
+// start server with port
 app.listen(port, () =>
 console.log(`Example app listening at http://localhost:${port}`));
