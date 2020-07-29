d5edcf56f2e01ea5e732a719911cc492a518ae5d
khoa <agnus1904@gmail.com>
2020-07-30 02:11:56 +0700

    new file controller, route and db.js

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-var a = [
-    "abc",
-    "abd",
-    "abe"
-];
-
+var userRoutes = require("./route/user.route");
+// use pug engine
 app.set('views', './views');
 app.set('view engine', 'pug');
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
+//use req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => res.render("index"));
 
-app.get("/users", function(req,res){
-    res.render("users/index",{lists: a});
-});
+app.use("/users",userRoutes);
 
-app.listen(port, () => 
+app.listen(port, () =>
 console.log(`Example app listening at http://localhost:${port}`));
